ec0c4c66c3fd87462ed901f5a7cf9b6f60d6fac4
AnisMouniym <95011291+AnisMouniym@users.noreply.github.com>
2022-04-06 16:16:21 +0200

    hidden feature update

--- application.js
+++ application.js
@@ -2,6 +2,14 @@
 document.getElementById("newgame")
         .addEventListener("click", function() {
   document.getElementById("home").hidden = true;
+  document.getElementById("game").hidden = false;
 }, false);
+
+document.getElementById("retour")
+        .addEventListener("click", function() {
+  document.getElementById("game").hidden = true;
+  document.getElementById("home").hidden = false;
+}, false);
+
 
 
