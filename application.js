910cf43e4a7ae59fd56bfd5a0d6ca2432075082f
AnisMouniym <95011291+AnisMouniym@users.noreply.github.com>
2022-04-06 15:16:57 +0200

    hide feature

--- application.js
+++ application.js
@@ -1,5 +1,7 @@
+
 document.getElementById("newgame")
         .addEventListener("click", function() {
   document.getElementById("home").hidden = true;
-  document.getElementById("game").hidden = false;
 }, false);
+
+
