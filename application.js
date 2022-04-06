0419634134ae01e8dfaa1db274c127a35683ec76
AnisMouniym <95011291+AnisMouniym@users.noreply.github.com>
2022-04-06 17:18:18 +0200

    hidden feature finished

--- application.js
+++ application.js
@@ -1,14 +1,19 @@
+let d1 = document.getElementById("logo")
 
 document.getElementById("newgame")
         .addEventListener("click", function() {
   document.getElementById("home").hidden = true;
   document.getElementById("game").hidden = false;
+  d1.style.display = "none";
+  
+  
 }, false);
 
-document.getElementById("retour")
+document.getElementById("accueil")
         .addEventListener("click", function() {
   document.getElementById("game").hidden = true;
   document.getElementById("home").hidden = false;
+  d1.style.display = "flex";
 }, false);
 
 
---
+++ pages.js
@@ -0,0 +1,16 @@
+export default function changerPage(page) {
+  if (page === "accueil") {
+    return `
+      <div id="grille">
+
+      </div>
+    `;
+  }
+  else if (page === "jeu") {
+    return `
+      <div>
+      
+      </div>
+    `;
+  }
+}
