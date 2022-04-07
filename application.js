790c8846add558f2966b8c7657beb967401a0318
AnisMouniym <95011291+AnisMouniym@users.noreply.github.com>
2022-04-07 11:17:45 +0200

    update hidden

--- application.js
+++ application.js
@@ -4,6 +4,7 @@ document.getElementById("newgame")
         .addEventListener("click", function() {
   document.getElementById("home").hidden = true;
   document.getElementById("game").hidden = false;
+  document.getElementById("surprise").hidden = true;
   d1.style.display = "none";
   
   
@@ -11,10 +12,25 @@ document.getElementById("newgame")
 
 document.getElementById("accueil")
         .addEventListener("click", function() {
-  document.getElementById("game").hidden = true;
-  document.getElementById("home").hidden = false;
-  d1.style.display = "flex";
+          if (confirm("Voulez-vous vraiment quitter la partie ?")) {
+            document.getElementById("home").hidden = false;
+            document.getElementById("game").hidden = true;
+            document.getElementById("surprise").hidden = false;
+            d1.style.display = "flex";
+          }
 }, false);
 
+document.getElementById("recomencer")
+        .addEventListener("click", function() {
+          if (confirm("Voulez-vous vraiment recommencer la partie ?")) {
+            document.getElementById("home").hidden = false;
+            document.getElementById("game").hidden = true;
+            document.getElementById("surprise").hidden = false;
+            d1.style.display = "flex";
+          }
+}, false);
+
+
+//fonction qui demande de confirmer oui ou non lorsque l'on clique sur le bouton "acceuil"
 
 
